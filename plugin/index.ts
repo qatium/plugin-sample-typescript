ca512c52b843b1aff228f6eeccd209fec7238bf7
Alex Torres <alex.torres@qatium.com>
2024-02-20 14:10:51 +0100

    Add sdk.map.addOverlay example

--- plugin/index.ts
+++ plugin/index.ts
@@ -1,16 +1,30 @@
-import { AssetStatus, PluginI, SDK, ValveFamilies } from "@qatium/plugin";
+import { AssetStatus, OverlayLayer, PluginI, SDK, ValveFamilies } from "@qatium/plugin";
 
 class Plugin implements PluginI {
   onNetworkChanged() {
-    console.log("network changed")
+    console.log("network changed");
   }
 
   run(sdk: SDK) {
+    const hydrants = sdk.network.getJunctions((j) => j.group === 'hydrant');
+
+    sdk.map.addOverlay([
+      {
+        type: 'HeatmapLayer',
+        id: 'density',
+        data: hydrants.map((h) => ({ geometry: h.geometry, properties: {}, type: 'Feature' })),
+        opacity: 0.5,
+        visible: true,
+        getPosition: (j) => j.geometry.coordinates,
+        radiusPixels: 25
+      } as OverlayLayer<"HeatmapLayer">
+    ]);
+
     sdk.ui.sendMessage(sdk.network.getValves((a) => (
       a.family === ValveFamilies.TCV &&
       !!a.simulation &&
       a.simulation.status === AssetStatus.CLOSED
-    )).length)
+    )).length);
   }
 
   onMessage(sdk: SDK, message: unknown) {
