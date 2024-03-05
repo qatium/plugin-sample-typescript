dbcf094d8e6a942f141dbe46996e2b0ef262a732
Alex Torres <alex.torres@qatium.com>
2024-03-05 08:14:15 +0100

    Expose etag header

--- vite.config.ui.ts
+++ vite.config.ui.ts
@@ -1,18 +1,18 @@
 import { defineConfig } from "vite"
 import { viteSingleFile } from "vite-plugin-singlefile"
-import { ViteEjsPlugin } from "vite-plugin-ejs";
 
 export default defineConfig(() => {
   return {
     build: {
       emptyOutDir: false
     },
-    plugins: [viteSingleFile(), ViteEjsPlugin(), {
+    plugins: [viteSingleFile(), {
       name: "configure-response-headers",
       configureServer: server => {
         server.middlewares.use((_req, res, next) => {
           res.setHeader("Access-Control-Request-Private-Network", "true");
           res.setHeader("Access-Control-Allow-Private-Network", "true");
+          res.setHeader("Access-Control-Expose-Headers", "ETag")
           next();
         });
       }
