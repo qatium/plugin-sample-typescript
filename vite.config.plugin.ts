3e58b4c0c56ec262ccc96d7bb42c40e3bc672c92
Alex Torres <alex.torres@qatium.com>
2024-03-27 09:35:40 +0100

    UPDATE plugin file naming

--- vite.config.plugin.ts
+++ vite.config.plugin.ts
@@ -8,7 +8,7 @@ export default defineConfig(() => {
       lib: {
         entry: "./plugin/index.ts",
         name: "test",
-        fileName: "plugin",
+        fileName: "engine",
         formats: ["es"] as LibraryFormats[]
       }
     }
